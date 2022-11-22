20eb35dd1fff48e8b8412818985b275d68b7bb4f
unknown <anna.sh.pb@gmail.com>
2022-11-22 22:11:57 +0200

    woo commerce support activated

--- functions.php
+++ functions.php
@@ -7,3 +7,5 @@ function bookstore_styles()
 	wp_enqueue_style('bootstrap-grid', get_bloginfo('template_directory') . '/styles/bootstrap/bootstrap-grid.css.map');
 }
 add_action('wp_head', 'bookstore_styles');
+
+add_theme_support( 'woocommerce' );
