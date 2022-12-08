bff46baf331420f6353a70dcbc96cbbb035f2643
unknown <anna.sh.pb@gmail.com>
2022-12-08 16:25:44 +0200

    shop archive template configured

---
+++ sidebar.php
@@ -0,0 +1,7 @@
+<aside id="secondary" class="primary-sidebar widget-area <?php if ($found) echo 'static'; ?>" role="complementary">
+	<section class="container sidebar-container">
+		<div class="sidebar-container sidebar-container-desktop">
+			<?php dynamic_sidebar('sidebar'); ?>
+		</div>
+	</section>
+</aside><!-- #secondary -->
